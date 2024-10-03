6da85f911105b954166baf7ae8035041ece17026
CenaBabajannjhad <cenababajannjhad@gmail.com>
2024-10-03 17:53:04 +0330

    send the user url to backend finished

---
+++ static/script.js
@@ -0,0 +1,47 @@
+const form = document.querySelector("#ytdl-form");
+const formInput = document.querySelector("#form-input");
+
+
+
+// convert url to base64
+const convertUrlToBase64 = url => {
+    return btoa(url)
+}
+// make full url
+const makeUrl = url => { 
+    let base64 = convertUrlToBase64(url);
+    let URL = `http://127.0.0.1:5000/api/${base64}`
+    return URL;
+}
+// fetch data
+const formHandler = e => {
+    e.preventDefault();
+    // get input value
+    let inputValue = formInput.value.trim(); 
+    // send input vlaue for makeUrl function
+    let URL = makeUrl(inputValue);
+    // send url for fetching data
+    fetchData(URL);
+}
+// event when form submited , call formHandler function
+form.addEventListener('submit' , formHandler);
+
+// fetch data , now just send a request
+const fetchData = async (url) => {
+    try{
+        let res = await fetch(url , {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({link : url})
+        })
+        console.log(res);
+
+    }catch(e){
+        console.error('error is : ' , e);
+    }
+}
+
+
+
